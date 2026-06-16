cb88591f7aaacac19c0591223c8b89b2dfc90037
brvale05 <brvale05@gmail.com>
2026-06-16 11:19:44 -0300

    first commit

---
+++ src/main/java/com/sistema/nutri/NutriApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.nutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriApplication.class, args);
+	}
+
+}
